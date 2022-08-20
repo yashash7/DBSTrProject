9046b155bc84b7b65fce42d610022639d5e422cb
Lokesh <85234503+Lokesh1606@users.noreply.github.com>
2022-08-20 09:17:20 +0000

    Add files via upload

---
+++ src/main/java/com/dbs/team3/Team3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3BackendApplication.class, args);
+	}
+
+}
